90bacc7cd20f4d8a9019b661a3194312f59ea3f5
jacobtnyoung <CRJ605@asu.edu>
2025-05-22 08:59:56 -0700

    lab 3 edits

--- labs/lab-automator-script.R
+++ labs/lab-automator-script.R
@@ -7,21 +7,10 @@ library( here )
 
 
 # create the instructions
-render( here( "labs/lab-01-instructions.Rmd" ), "html_document" )
-render( here( "labs/lab-02-instructions.Rmd" ), "html_document" )
-render( here( "labs/lab-03-instructions.Rmd" ), "html_document" )
-render( here( "labs/lab-04-instructions.Rmd" ), "html_document" )
-render( here( "labs/lab-05-instructions.Rmd" ), "html_document" )
-render( here( "labs/lab-06-instructions.Rmd" ), "html_document" )
-render( here( "labs/final-project-instructions.Rmd" ), "html_document" )
-
-
-# create the templates 
-# the reason here is to make sure they work before you convert them to zips
-#render( here( "labs/lab-02-template.Rmd" ), "html_document" )
-#render( here( "labs/lab-03-template.Rmd" ), "html_document" )
-#render( here( "labs/lab-04-template.Rmd" ), "html_document" )
-#render( here( "labs/lab-05-template.Rmd" ), "html_document" )
-#render( here( "labs/lab-06-template.Rmd" ), "html_document" )
-#render( here( "labs/final-project-template.Rmd" ), "html_document" )
-
+render( here( "labs/lab-01-instructions.qmd" ), "html_document" )
+render( here( "labs/lab-02-instructions.qmd" ), "html_document" )
+render( here( "labs/lab-03-instructions.qmd" ), "html_document" )
+render( here( "labs/lab-04-instructions.qmd" ), "html_document" )
+render( here( "labs/lab-05-instructions.qmd" ), "html_document" )
+render( here( "labs/lab-06-instructions.qmd" ), "html_document" )
+render( here( "labs/final-project-instructions.qmd" ), "html_document" )
